90176218f9fc4118e6afc93c4dc3d37a1ced8437
Miyakowww <ltq73618@163.com>
2022-01-09 17:40:15 +0800

    use function pointers instead of closures

--- tests/hello_aoi.rs
+++ tests/hello_aoi.rs
@@ -1,33 +1,34 @@
 use aoi::*;
 
+fn interrupt(id: u8, args: Vec<AoType>) -> Option<AoType> {
+    match id {
+        1 => {
+            match &args[0] {
+                AoType::AoBool(v) => print!("{}", v),
+                AoType::AoInt(v) => print!("{}", v),
+                AoType::AoFloat(v) => print!("{}", v),
+                AoType::AoString(v) => print!("{}", v),
+                _ => (),
+            }
+            None
+        }
+        2 => {
+            match &args[0] {
+                AoType::AoBool(v) => print!("{}\n", v),
+                AoType::AoInt(v) => print!("{}\n", v),
+                AoType::AoFloat(v) => print!("{}\n", v),
+                AoType::AoString(v) => print!("{}\n", v),
+                _ => (),
+            }
+            None
+        }
+        _ => None,
+    }
+}
+
 #[test]
 fn hello_aoi() {
-    let mut vm = AoVM::new(
-        |id, para| match id {
-            1 => {
-                match &para[0] {
-                    AoType::AoBool(v) => print!("{}", v),
-                    AoType::AoInt(v) => print!("{}", v),
-                    AoType::AoFloat(v) => print!("{}", v),
-                    AoType::AoString(v) => print!("{}", v),
-                    _ => (),
-                }
-                None
-            }
-            2 => {
-                match &para[0] {
-                    AoType::AoBool(v) => print!("{}\n", v),
-                    AoType::AoInt(v) => print!("{}\n", v),
-                    AoType::AoFloat(v) => print!("{}\n", v),
-                    AoType::AoString(v) => print!("{}\n", v),
-                    _ => (),
-                }
-                None
-            }
-            _ => None,
-        },
-        0,
-    );
+    let mut vm = AoVM::new(interrupt, 0);
     let program = vec![
         AoOpCode::PUSH(AoArg::DSB),                // push dsb
         AoOpCode::PUSH(AoArg::from("Hello Aoi!")), // push "Hello Aoi!"
